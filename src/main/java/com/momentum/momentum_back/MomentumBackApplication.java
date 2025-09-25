6f5c8c330b1d37e56e08fb58b0fc82a0300020c1
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-09-25 14:54:15 -0500

    feat: init project

---
+++ src/main/java/com/momentum/momentum_back/MomentumBackApplication.java
@@ -0,0 +1,13 @@
+package com.momentum.momentum_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentumBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentumBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momentum/momentum_back/MomentumBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momentum.momentum_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentumBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
